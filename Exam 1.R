aed1c1e46c9e6c342fbf50dba574d633c56ba0f7
Luke Boagni <lukeboagni@Lukes-MacBook-Pro.local>
2019-09-18 14:54:44 -0500

    Operators

---
+++ Exam 1.R
@@ -0,0 +1,32 @@
+#Script 1
+a = seq(from = 5, to=14, by=1)
+b=a[1]
+c=a[7]
+b>c
+b<c
+b>=c
+x=a[2]
+y=a[6]
+z=a[9]
+
+((z+x)*(z+y))/2
+10*(x-y)
+#the R operator for negation is !
+b%%c #modulus function
+b^c #exponential function
+b%/%c #integer division function
+
+"%lukesop%"=function(a,b){(a*2)+(b*3)}
+b%lukesop%c
+
+"me"->g
+
+h=1;i=2;j=3
+h;i;j
+
+2^4000 #higher number than console can compute
+-2^4000 #-inf
+0/0 #nan = Not a number
+k=NA #Object with no value assigned
+
+
